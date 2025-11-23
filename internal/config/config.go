94405678b707fff6b1f7d17305e7569150f2d02f
prestonfoshee <59655377+prestonfoshee@users.noreply.github.com>
2025-11-23 15:10:31 -0500

    create main config module

---
+++ internal/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+// Config is the main configuration struct for the application
+// Will create these, but this is the placeholder for now
+// type Config struct {
+// 	Server ServerConfig `mapstructure:"server"`
+// 	Database DatabaseConfig `mapstructure:"database"`
+// 	Spotify SpotifyConfig `mapstructure:"spotify"`
+// 	JWT JWTConfig `mapstructure:"jwt"`
+// }
